f0b385291c1624619e7906bd0aec758b4d219ea3
Reiner Huechting <reinerh-dev@pm.me>
2026-01-23 13:30:02 +0100

    Beispiel für Benutzung des Wörterbuchs hinzugefügt.

---
+++ main/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"dictionary/dict"
+	"dictionary/entry"
+	"fmt"
+)
+
+func main() {
+	e1 := entry.New("Haus", "House")
+	e2 := entry.New("Fahrrad", "bicycle")
+
+	d := dict.New()
+	d.Add(e1)
+	d.Add(e2)
+
+	fmt.Print("Bitte ein deutsches Wort eingeben: ")
+	de := ""
+	fmt.Scanln(&de)
+
+	en := d.Lookup(de)
+	fmt.Printf("Das englische Wort ist: %s\n", en)
+}
